159c9d2f7c581407e9e426c097ce6b90dee3285f
John Montgomery <john@littlespikeyland.com>
2010-02-23 20:27:49 +0000

    more admin configuration

--- background_task/admin.py
+++ background_task/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    display_filter = ['task_name']
+    list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts']
+
+admin.site.register(Task, TaskAdmin)
