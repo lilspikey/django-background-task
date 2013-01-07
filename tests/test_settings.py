c035fb729f4fd2d7c8e4f24badde3f9b994d075d
John Montgomery <john@littlespikeyland.com>
2013-01-07 09:43:02 +0000

    Not requiring django-coverage unless running coverage tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -7,8 +7,9 @@ DATABASE_NAME = ':memory:'
 INSTALLED_APPS = [ 'background_task' ]
 
 
-# http://pypi.python.org/pypi/django-coverage
+if 'test_coverage' in sys.argv:
+    # http://pypi.python.org/pypi/django-coverage
+    INSTALLED_APPS.append('django_coverage')
+    COVERAGE_REPORT_HTML_OUTPUT_DIR = 'html_coverage'
+    COVERAGE_MODULE_EXCLUDES = []
 
-INSTALLED_APPS.append('django_coverage')
-COVERAGE_REPORT_HTML_OUTPUT_DIR = 'html_coverage'
-COVERAGE_MODULE_EXCLUDES = []
